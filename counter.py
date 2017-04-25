74d630b6cf40cc3ef525d0762e1f10cea0a622d9
Mihir Kulkarni <mihirskulkarni@gmail.com>
2017-04-25 11:54:03 -0400

    Update counter.py

--- counter.py
+++ counter.py
@@ -11,7 +11,7 @@ cons.extend(['\u092' + str(i) for i in range(10)] + ['\u092' + chr(i) for i in r
 cons.extend(['\u093' + str(i) for i in range(10)])
 cons_u = [unicode(i, 'unicode-escape') for i in cons]
 
-final_ct = {i: ct[i] for i in cons_u}   #Creating dictionary of counts of only Devanagari characters
+final_ct = {i: ct[i] for i in cons_u}   #Creating dictionary of counts of only Devanagari consonants
 final_ct_sorted_keys = sorted(final_ct, key=final_ct.get, reverse=True)     #Arranging with counts
 for r in final_ct_sorted_keys[:10]:
     print r, final_ct[r]
